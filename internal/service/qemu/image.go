c1172dc7541248737b89583786b2ac5090a82ec3
Đậu Khoa <khoakomlem@gmail.com>
2025-03-22 00:29:26 +0700

    rename

--- internal/service/qemu/image.go
+++ internal/service/qemu/image.go
@@ -16,12 +16,13 @@ func NewService(repo *repository.Repository) *Service {
 	return &Service{repo: repo}
 }
 
-func (s *Service) ImageCreate(baseImgPath string, cloneImgPath string) error {
+func (s *Service) CreateImage(baseImgPath string, cloneImgPath string) error {
 	if !fileExists(baseImgPath) {
 		return fmt.Errorf("base image not found")
 	}
 
-	cmd := exec.Command("qemu-img", // name
+	// Eg: qemu-img create -b ubuntu_amd64.img -f qcow2 -F qcow2 ubuntu_amd64_mod.img 10G
+	cmd := exec.Command("qemu-img",
 		"create",
 		"-b",
 		baseImgPath,
@@ -30,7 +31,7 @@ func (s *Service) ImageCreate(baseImgPath string, cloneImgPath string) error {
 		"-F",
 		"qcow2",
 		cloneImgPath,
-		"2G",
+		"10G", // TODO: add volumn params
 	)
 
 	if err := cmd.Run(); err != nil {
@@ -39,6 +40,29 @@ func (s *Service) ImageCreate(baseImgPath string, cloneImgPath string) error {
 
 	return nil
 }
+
+// func (s *Service) Convert(imgPath string, format string, destPath string) error {
+// 	if !fileExists(imgPath) {
+// 		return fmt.Errorf("image not found")
+// 	}
+
+// 	// Eg. qemu-img convert -f qcow2 -O raw focal-server-cloudimg-amd64.img focal-server-cloudimg-amd64.raw
+// 	cmd := exec.Command("qemu-img", // name
+// 		"convert",
+// 		"-f",
+// 		"qcow2",
+// 		"-O",
+// 		format,
+// 		imgPath,
+// 		destPath,
+// 	)
+
+// 	if err := cmd.Run(); err != nil {
+// 		return fmt.Errorf("failed to convert image: %s", err)
+// 	}
+
+// 	return nil
+// }
 
 // func ImageResize(imgPath string, vol *Volumn) error {
 // 	if !fileExists(imgPath) {
