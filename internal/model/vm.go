640effc159d6e598ff92c62faf8db17aa52e0753
Đậu Khoa <khoakomlem@gmail.com>
2025-04-07 23:53:59 +0700

    add model vm

--- internal/model/vm.go
+++ internal/model/vm.go
@@ -1,6 +1,13 @@
-
-
 package model
 
-
-
+type VM struct {
+	ID        int64  `json:"id"`
+	AccountID int64  `json:"account_id"`
+	NetworkID string `json:"network_id"`
+	OsID      string `json:"os_id"`
+	ArchID    string `json:"arch_id"`
+	Name      string `json:"name"`
+	Cpu       int32  `json:"cpu"`
+	Ram       int32  `json:"ram"`     // In MB
+	Storage   int32  `json:"storage"` // In GB
+}
