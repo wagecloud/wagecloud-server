95450122aa129ad8e6be2c44f681ccd62494e7d6
khoakomlem <khoakomlem@gmail.com>
2025-04-10 02:08:01 +0700

    move cloudinit service to libvirt

--- internal/service/service.go
+++ internal/service/service.go
@@ -3,32 +3,28 @@ package service
 import (
 	"github.com/wagecloud/wagecloud-server/internal/repository"
 	"github.com/wagecloud/wagecloud-server/internal/service/account"
-	"github.com/wagecloud/wagecloud-server/internal/service/cloudinit"
 	"github.com/wagecloud/wagecloud-server/internal/service/libvirt"
 	"github.com/wagecloud/wagecloud-server/internal/service/qemu"
 	"github.com/wagecloud/wagecloud-server/internal/service/vm"
 )
 
 type Service struct {
-	Account   account.ServiceInterface
-	Cloudinit cloudinit.ServiceInterface
-	Libvirt   libvirt.ServiceInterface
-	Qemu      qemu.ServiceInterface
-	VM        vm.ServiceInterface
+	Account account.ServiceInterface
+	Libvirt libvirt.ServiceInterface
+	Qemu    qemu.ServiceInterface
+	VM      vm.ServiceInterface
 }
 
 func New(repo *repository.RepositoryImpl) *Service {
 	accountSvc := account.NewService(repo)
-	cloudinitSvc := cloudinit.NewService(repo)
 	libvirtSvc := libvirt.NewService(repo)
 	qemuSvc := qemu.NewService(repo)
-	vmSvc := vm.NewService(repo)
+	vmSvc := vm.NewService(repo, libvirtSvc)
 
 	return &Service{
-		Account:   accountSvc,
-		Cloudinit: cloudinitSvc,
-		Libvirt:   libvirtSvc,
-		Qemu:      qemuSvc,
-		VM:        vmSvc,
+		Account: accountSvc,
+		Libvirt: libvirtSvc,
+		Qemu:    qemuSvc,
+		VM:      vmSvc,
 	}
 }
